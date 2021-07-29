0de7d82cee97e121766f7c25050baece9caccd88
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-07-29 18:24:23 +0300

    Add function that loops through the array and displays each book on the page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-let myLibrary = [];
+const myLibrary = [];
+
+const allBooks = document.getElementById('bookList');
 
 function Book(name, author, pages, read) {
   this.name = name;
@@ -7,6 +9,21 @@ function Book(name, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function createBookCard(book) {
+  const div = document.createElement('div');
+  const p = document.createElement('p');
+  p.textContent = book.title;
+  div.appendChild(p);
+  return div;
+}
+
+function displayAllBooks() {
+  for (const book of myLibrary) {
+    const bookCard = createBookCard(book);
+    allBooks.appendChild(bookCard);
+  }
 }
