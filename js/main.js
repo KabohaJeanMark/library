f851fafa26fc008eeb2cde681bbb50db0863a407
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-07-30 20:55:55 +0300

    Refactor to use event listener instead of just onclick and add prevent default for form submission

--- js/main.js
+++ js/main.js
@@ -2,10 +2,6 @@ const myLibrary = [];
 
 const allBooks = document.getElementById('bookList');
 
-const addBookbtn = document.querySelector('#createBookBtn');
-
-addBookbtn.onclick = addBookToLibrary();
-
 function Book(name, author, pages, read) {
   this.name = name;
   this.author = author;
@@ -18,20 +14,24 @@ function addBookToLibrary() {
   const author = document.getElementById('author').value;
   const pages = document.getElementById('pages').value;
   let read;
-  if (document.getElementById('alreadyRead').checked) {
-    read = document.getElementById('alreadyRead').value;
+  const rbs = document.querySelectorAll('input[name="read"]');
+  for (const rb of rbs) {
+    if (rb.checked) {
+      read = rb.value;
+      break;
+    }
   }
-  else {
-    read = document.getElementById('notRead').value;
-  }
-  addedBook = new Book(name, author, pages, read);
+  const addedBook = new Book(name, author, pages, read);
   myLibrary.push(addedBook);
 }
 
 function createBookCard(book) {
   const div = document.createElement('div');
   const p = document.createElement('p');
-  p.textContent = book.title;
+  const p2 = document.createElement('p');
+  console.log(book);
+  p.textContent = book.name;
+  p2.textContent = 'book 1';
   div.appendChild(p);
   return div;
 }
@@ -42,3 +42,12 @@ function displayAllBooks() {
     allBooks.appendChild(bookCard);
   }
 }
+
+const addForm = document.getElementById('createForm');
+addForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+});
+
+const getAllBoooks = document.querySelector('#allBooks');
+getAllBoooks.addEventListener('click', displayAllBooks);
