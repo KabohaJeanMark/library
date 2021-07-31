a678b2071dcb634b0ec589bc633fefbc897ed9e4
KabohaJeanMark <kabohajeanmark@gmail.com>
2021-07-31 11:07:50 +0300

    Enable book creation to refresh the display. Fix error of update and delete being called without clicking on the buttons

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const myLibrary = [
-  // {
-  //   name: 'Harry Porter',
-  //   author: 'J.K Rowling',
-  //   pages: '200',
-  //   read: 'yes'
-  // }
+  {
+    name: 'Harry Porter',
+    author: 'J.K Rowling',
+    pages: '200',
+    read: 'yes',
+  },
 ];
 
 const allBooks = document.getElementById('bookList');
@@ -30,58 +30,65 @@ function addBookToLibrary() {
   }
   const addedBook = new Book(name, author, pages, read);
   myLibrary.push(addedBook);
-}
-
-function deleteBook(book) {
-  const bookIndex = myLibrary.indexOf(book);
-  delete myLibrary[bookIndex];
-  console.log('The book has been deleted')
-}
-
-function updateStatus(book) {
-  console.log(book);
-  if (book.read== 'yes') {
-    book.read = 'no';
-  }
-  else {
-    book.read = 'yes';
-  }
-  console.log(book);
+  console.log(myLibrary);
+  allBooks.textContent = '';
+  displayAllBooks();
 }
 
 function createBookCard(book) {
   const div = document.createElement('div');
-  div.classList.add('m-2','p-1')
+  div.classList.add('m-2', 'p-1');
   const p = document.createElement('p');
   p.textContent = book.name;
   const deleteBtn = document.createElement('button');
-  deleteBtn.classList.add('btn','btn-danger','m-1'); 
+  deleteBtn.classList.add('btn', 'btn-danger', 'm-1');
   deleteBtn.textContent = 'DELETE';
   const updateBtn = document.createElement('button');
-  updateBtn.classList.add('btn','btn-warning','m-1');
+  updateBtn.classList.add('btn', 'btn-warning', 'm-1');
   updateBtn.textContent = 'UPDATE';
   div.appendChild(p);
   div.appendChild(deleteBtn);
   div.appendChild(updateBtn);
-  deleteBtn.addEventListener('click', deleteBook(book));
-  updateBtn.addEventListener('click',updateStatus(book));
+  updateBtn.addEventListener('click', () => {
+    updateStatus(book);
+  });
+  deleteBtn.addEventListener('click', () => {
+    deleteBook(book);
+  });
   return div;
 }
 
 function displayAllBooks() {
   if (!myLibrary.length) {
-    console.log('There are no books in library')
+    console.log('There are no books in library');
     const p2 = document.createElement('p');
     p2.textContent = 'There are currently no books';
     allBooks.appendChild(p2);
-  }
-  else {
+  } else {
     for (const book of myLibrary) {
       const bookCard = createBookCard(book);
       allBooks.appendChild(bookCard);
+      allBooks.style.display = 'block';
     }
   }
-  
+}
+
+function deleteBook(book) {
+  const bookIndex = myLibrary.indexOf(book);
+  delete myLibrary[bookIndex];
+  console.log('The book has been deleted by default if I havent pressed the button yet');
+  displayAllBooks();
+}
+
+function updateStatus(book) {
+  console.log(book);
+  if (book.read === 'yes') {
+    book.read = 'no';
+  } else {
+    book.read = 'yes';
+  }
+  console.log('The book has been updated by default if I havent pressed the button yet');
+  console.log(book);
 }
 
 const addForm = document.getElementById('createForm');
